843015d9411fa62c7b6ef3946b4f456d7798c38d
roxor <riskyvian@gmail.com>
2017-12-07 23:29:57 +0700

    finish usualn

--- include/view/read/usulan_terbaik.php
+++ include/view/read/usulan_terbaik.php
@@ -1,166 +1,68 @@
 <div class="col-sm-6 col-sm-offset-4">  
-<?php
-    $d=1;
-    $x=array();
-    $dd=0;
-    $y=array();
-
-     mysqli_query($db_link,"CREATE TEMPORARY TABLE rangking(Rangking INT AUTO_INCREMENT primary key
-		,no_pegawai varchar(8)
-		,nama varchar(50)
-        ,nilai decimal(10,8));");
-
-
-
-        $nilai_ypositif=array();
-        $temp_positif=NULL;
-        $temp_negatif=NULL;
-        
-        $nilai_ynegatif=array();
-        $id_jabatan=array();
-    //mengambil data kriteria
-    $kriteria=mysqli_query($db_link,"SELECT id_kriteria,atribut FROM kriteria ");
-    while($data_kriteria=mysqli_fetch_assoc($kriteria)){
-        //per kriteria
-        //masih seluruh pegawai belum ada filter
-        $sqlfornilai="SELECT C.no_pegawai,C.nama,A.nilai,D.akumulasi FROM penilaian A
-                                        INNER JOIN jabatan_pegawai B ON A.id_jabatan=B.id_jabatan
-                                        INNER JOIN pegawai C ON B.id_pegawai=C.no_pegawai
-                                        INNER JOIN bobot_penilaian D ON A.id_bobot=D.id_bobot
-                                        WHERE D.id_kriteria='".$data_kriteria['id_kriteria']."'
-                                      ORDER BY C.no_pegawai";
-    $nilai_krit=mysqli_query($db_link,$sqlfornilai);                                  
-    $nilai_krit2=mysqli_query($db_link,$sqlfornilai);
-         
-        $nilai=NULL;
-
-        //nilai per pegawai per kriteria
-        while($data_nilai=mysqli_fetch_assoc($nilai_krit)){
-                //nilai merupakan hasil pangkat
-                $nilai=$nilai+pow($data_nilai['nilai'],2);   
-        }
-        $x[$d]=sqrt($nilai);
-        $e=1;
-        $ee=0;
-        
-               
-        //matrix keputusan normalisasi per kriteria
-         while($data_nilai=mysqli_fetch_assoc($nilai_krit2)){
-                //nilai merupakan hasil pangkat
-                $y[$d][$e]=($data_nilai['nilai']/$x[$d])*$data_nilai['akumulasi'];   
-  
-         //mencari nilai Y;
-            if($data_kriteria['atribut']=='K'){
-               if(empty($nilai_ypositif[$d])){
-                   $nilai_ypositif[$d]=$y[$d][$e];
-               }
-               if(empty($nilai_ynegatif[$d])){
-                   $nilai_ynegatif[$d]=$y[$d][$e];
-               }
-                if( $y[$d][$e]> $nilai_ypositif[$d]){
-                    $nilai_ypositif[$d]=$y[$d][$e];
-                }
-                 if( $y[$d][$e]< $nilai_ynegatif[$d]){
-                    $nilai_ynegatif[$d]=$y[$d][$e];
-                }
-            }
-            if($data_kriteria['atribut']=='B'){
-               if(empty($nilai_ypositif[$d])){
-                   $nilai_ypositif[$d]=$y[$d][$e];
-               }
-               if(empty($nilai_ynegatif[$d])){
-                   $nilai_ynegatif[$d]=$y[$d][$e];
-               }
-                if( $y[$d][$e]< $nilai_ypositif[$d]){
-                    $nilai_ypositif[$d]=$y[$d][$e];
-                }
-                 if( $y[$d][$e]> $nilai_ynegatif[$d]){
-                    $nilai_ynegatif[$d]=$y[$d][$e];
-                }
-            }
-            $nama_pegawai[$e]=$data_nilai['nama'];
-            $no_pegawai[$e]=$data_nilai['no_pegawai'];
-        $ee=$e;
-        $e++;
-        }
-       
-
-        $dd=$d;
-        
-        $d++;
-    }
-
-   
-    //mencari nilai D;
- 
-    $d_plus=array();
-    $d_minus=array();
-    $v=array();
-
-    echo '<br>';
-    for ($zz=1;$zz<=$ee;$zz++){
-        $d_plus[$zz]=NULL;
-        $d_minus[$zz]=NULL;
-        $v[$zz]=NULL;
-        
-        for($nn=1;$nn<=$dd;$nn++){
-            //echo $y[$nn][$ee];
-
-        $d_plus[$zz]=$d_plus[$zz]+pow($nilai_ypositif[$nn]-$y[$nn][$zz],2);
-               
-        $d_minus[$zz]=$d_minus[$zz]+pow($nilai_ynegatif[$nn]-$y[$nn][$zz],2);
-           //echo $nilai_ynegatif[$nn].'|'.$y[$nn][$zz].'<br>';
-        }
-
-        $d_plus[$zz]=sqrt($d_plus[$zz]);
-        $d_minus[$zz]=sqrt($d_minus[$zz]);
-  
-        //nilai preferensi alternatif
-        //echo $d_minus[$zz].'/'.$d_minus[$zz].'+'.$d_plus[$zz].'<br>';
-        $v[$zz]=$d_minus[$zz]/($d_minus[$zz]+$d_plus[$zz]);
-        $queryfinish=mysqli_query($db_link,"INSERT INTO rangking (no_pegawai,nama,nilai) 
-        VALUES ('".$no_pegawai[$zz]."','".$nama_pegawai[$zz]."',".$v[$zz].")");
-    }
-
+<?php 
+$toko=mysqli_query($db_link,"SELECT id_toko,nama_toko FROM toko");
+$bag=mysqli_query($db_link,"SELECT id_bagian,bagian FROM bagian");  
 ?>
 
 <h2 class="text-center">LAPORAN USULAN PEGAWAI TERBAIK</h2> 
 	<div class="panel-group" >
 		<div class="panel panel-default" style="padding:10px" >
             <br/>
-<?php   
-
-$sql_rangking="SELECT no_pegawai,nama,nilai FROM rangking ORDER BY nilai DESC ";
-$hasil_rangking=mysqli_query($db_link,$sql_rangking);
-$total_rangking=mysqli_num_rows($hasil_rangking);
-
-        echo '<table class="table table-bordered table-hover text-center panel panel-primary" >
+            <form class="form-horizontal">
+             <div class="form-group">
+                <label class="control-label col-sm-3" for="toko">Toko : </label>
+                <div class="col-sm-5">
+                    <select  class="form-control" name="toko">  
+                        <option value='0'>All</option>
+                    <?php
+                        while ($data_toko=mysqli_fetch_assoc($toko)){
+                            echo "<option value='".$data_toko['id_toko']."'>".$data_toko['nama_toko']."</option>";
+                        }
+                    ?>
+                </select> 
+                </div>
+            </div>
+             <div class="form-group">
+                <label class="control-label col-sm-3" for="jabatan">Jabatan : </label>
+                <div class="col-sm-5">
+                        <select  class="form-control" name="jabatan" id="jabatan">  
+                        <option value="none">All</option>
+                        <option value="koordinator">Koordinator</option>
+                        <option value="karyawan">Karyawan</option>
+                    </select> 
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-3" for="bagian">Bagian : </label>
+                <div class="col-sm-5">
+                    <select  class="form-control" name="bagian">  
+                        <option value='none' >All</option>
+                    <?php
+                        while ($data_bag=mysqli_fetch_assoc($bag)){
+                            echo "<option value='".$data_bag['id_bagian']."'>".$data_bag['bagian']."</option>";
+                        }
+                    ?>
+                </select> 
+                </div>
+           </div>
+           <div class="form-group">
+                <label class="control-label col-sm-3" for="toko">Jumlah Terbaik : </label>
+                <div class="col-sm-5">
+                   <input type="number" name='jumlah_terbaik' class="form-control" >
+                </div>
+           </div>
+           <div class="text-center">	
+					<button type="button" id="tampil" class="btn btn-success">TAMPIL</button>
+				</div>
                     
-                <thead class="panel-heading">
-                <tr>
-                    <th class="text-center" rowspan="2" style="vertical-align: middle;">RANGKING</th>
-                    <th class="text-center" rowspan="2" style="vertical-align: middle;">NO PEGAWAI</th>
-                    <th class="text-center" rowspan="2" style="vertical-align: middle;">NAMA PEGAWAI</th>
-                    <th class="text-center" rowspan="2" style="vertical-align: middle;">NILAI</th>
-                </tr>
-        </thead>
-        <tbody> ';
-        $s=1;
+            </form>
+            </div>
+            <br/>
+        <div class="point">
+        </div>
+    </tbody>
+    </table>
 
-        while ($data_rangking=mysqli_fetch_assoc($hasil_rangking)) {
-            echo "<tr>";
-            echo "  
-                <td>".$s."</td>
-                 <td>{$data_rangking['no_pegawai']}</td>
-                <td>{$data_rangking['nama']}</td>
-                <td>".$data_rangking['nilai']."</td>";
-            echo "</tr>";
-        $s++;
-        }
-    echo "</tbody></table>";
-
-?>
-	
 		</div>
 	</div>
 </div>
@@ -170,9 +72,20 @@ $total_rangking=mysqli_num_rows($hasil_rangking);
 <script>
 	 $(document).ready(function () {
 
-        $(".detail").click(function () {
-           		window.location.replace("index.php?navigasi=laporan_penilaian_pegawai&crud=detail");
-          });
-     
-	 });
+        $("#tampil").click(function () {
+            var id_toko= $('select[name=toko]').val();
+            var jabatan= $('select[name=jabatan]').val();
+            var id_bagian= $('select[name=bagian]').val();
+            var jum_terbaik= $('input[name=jumlah_terbaik]').val();
+           	$.ajax({
+					type: "POST",
+					url: "../include/view/read/usulan_terbaik2.php",
+					data: 'id_toko='+id_toko+'&jabatan='+jabatan+'&id_bagian='+id_bagian+'&jum_terbaik='+jum_terbaik,
+					success: function (respons) {
+                        $('.point').html(respons);
+                        
+                    }
+               });
+        });
+     });
 </script>
---
+++ include/view/read/usulan_terbaik2.php
@@ -0,0 +1,178 @@
+<?php
+    include_once "../../../koneksi.php";
+    $id_toko=$_POST['id_toko'];
+    $jabatan=$_POST['jabatan'];
+    $id_bagian=$_POST['id_bagian'];
+    $jum_terbaik=$_POST['jum_terbaik'];
+    $d=1;
+    $x=array();
+    $dd=0;
+    $y=array();
+
+     mysqli_query($db_link,"CREATE TEMPORARY TABLE rangking(Rangking INT AUTO_INCREMENT primary key
+		,no_pegawai varchar(8)
+		,nama varchar(50)
+        ,nilai decimal(10,8)
+        ,toko varchar(255)
+        ,jabatan varchar(8)
+        ,bagian varchar(255));");
+
+
+
+        $nilai_ypositif=array();
+        $temp_positif=NULL;
+        $temp_negatif=NULL;
+        
+        $nilai_ynegatif=array();
+        $id_jabatan=array();
+    //mengambil data kriteria
+    $kriteria=mysqli_query($db_link,"SELECT id_kriteria,atribut FROM kriteria ");
+    while($data_kriteria=mysqli_fetch_assoc($kriteria)){
+        //per kriteria
+        //masih seluruh pegawai belum ada filter
+        $sqlfornilai="SELECT C.no_pegawai,C.nama,A.nilai,D.akumulasi,E.nama_toko,B.jabatan,F.bagian FROM penilaian A
+                                        INNER JOIN jabatan_pegawai B ON A.id_jabatan=B.id_jabatan
+                                        INNER JOIN pegawai C ON B.id_pegawai=C.no_pegawai
+                                        INNER JOIN bobot_penilaian D ON A.id_bobot=D.id_bobot AND B.jabatan=D.jabatan
+                                        INNER JOIN toko E ON B.id_toko=E.id_toko
+                                        INNER JOIN bagian F ON B.id_bagian=F.id_bagian
+                                        WHERE D.id_kriteria='".$data_kriteria['id_kriteria']."'
+                                        AND E.id_toko=CASE WHEN ".$id_toko."=0 THEN E.id_toko ELSE ".$id_toko." END
+                                        AND B.jabatan=CASE WHEN '".$jabatan."'='none'THEN B.jabatan ELSE '".$jabatan."' END
+                                        AND F.id_bagian=CASE WHEN '".$id_bagian."'='none' THEN F.id_bagian ELSE '".$id_bagian."' END
+                                      ORDER BY C.no_pegawai";
+    $nilai_krit=mysqli_query($db_link,$sqlfornilai);                                  
+    $nilai_krit2=mysqli_query($db_link,$sqlfornilai);
+         if(!$nilai_krit){
+                 mysqli_errno($db_link);
+         }
+        $nilai=NULL;
+
+        //nilai per pegawai per kriteria
+        while($data_nilai=mysqli_fetch_assoc($nilai_krit)){
+                //nilai merupakan hasil pangkat
+                $nilai=$nilai+pow($data_nilai['nilai'],2);   
+        }
+        $x[$d]=sqrt($nilai);
+        $e=1;
+        $ee=0;
+        
+               
+        //matrix keputusan normalisasi per kriteria
+         while($data_nilai=mysqli_fetch_assoc($nilai_krit2)){
+                //nilai merupakan hasil pangkat
+                $y[$d][$e]=($data_nilai['nilai']/$x[$d])*$data_nilai['akumulasi'];   
+  
+         //mencari nilai Y;
+            if($data_kriteria['atribut']=='K'){
+               if(empty($nilai_ypositif[$d])){
+                   $nilai_ypositif[$d]=$y[$d][$e];
+               }
+               if(empty($nilai_ynegatif[$d])){
+                   $nilai_ynegatif[$d]=$y[$d][$e];
+               }
+                if( $y[$d][$e]> $nilai_ypositif[$d]){
+                    $nilai_ypositif[$d]=$y[$d][$e];
+                }
+                 if( $y[$d][$e]< $nilai_ynegatif[$d]){
+                    $nilai_ynegatif[$d]=$y[$d][$e];
+                }
+            }
+            if($data_kriteria['atribut']=='B'){
+               if(empty($nilai_ypositif[$d])){
+                   $nilai_ypositif[$d]=$y[$d][$e];
+               }
+               if(empty($nilai_ynegatif[$d])){
+                   $nilai_ynegatif[$d]=$y[$d][$e];
+               }
+                if( $y[$d][$e]< $nilai_ypositif[$d]){
+                    $nilai_ypositif[$d]=$y[$d][$e];
+                }
+                 if( $y[$d][$e]> $nilai_ynegatif[$d]){
+                    $nilai_ynegatif[$d]=$y[$d][$e];
+                }
+            }
+            $nama_pegawai[$e]=$data_nilai['nama'];
+            $no_pegawai[$e]=$data_nilai['no_pegawai'];
+            $toko[$e]=$data_nilai['nama_toko'];
+            $bag[$e]=$data_nilai['bagian'];
+            $jab[$e]=$data_nilai['jabatan'];
+        $ee=$e;
+        $e++;
+        }
+       
+
+        $dd=$d;
+        
+        $d++;
+    }
+
+   
+    //mencari nilai D;
+ 
+    $d_plus=array();
+    $d_minus=array();
+    $v=array();
+
+    echo '<br>';
+    for ($zz=1;$zz<=$ee;$zz++){
+        $d_plus[$zz]=NULL;
+        $d_minus[$zz]=NULL;
+        $v[$zz]=NULL;
+        
+        for($nn=1;$nn<=$dd;$nn++){
+            //echo $y[$nn][$ee];
+
+        $d_plus[$zz]=$d_plus[$zz]+pow($nilai_ypositif[$nn]-$y[$nn][$zz],2);
+               
+        $d_minus[$zz]=$d_minus[$zz]+pow($nilai_ynegatif[$nn]-$y[$nn][$zz],2);
+           //echo $nilai_ynegatif[$nn].'|'.$y[$nn][$zz].'<br>';
+        }
+
+        $d_plus[$zz]=sqrt($d_plus[$zz]);
+        $d_minus[$zz]=sqrt($d_minus[$zz]);
+  
+        //nilai preferensi alternatif
+        //echo $d_minus[$zz].'/'.$d_minus[$zz].'+'.$d_plus[$zz].'<br>';
+        $v[$zz]=$d_minus[$zz]/($d_minus[$zz]+$d_plus[$zz]);
+        $queryfinish=mysqli_query($db_link,"INSERT INTO rangking (no_pegawai,nama,nilai,toko,jabatan,bagian) 
+        VALUES ('".$no_pegawai[$zz]."','".$nama_pegawai[$zz]."',".$v[$zz].",'".$toko[$zz]."','".$jab[$zz]."','".$bag[$zz]."')");
+    }
+
+    //munculkan toko
+    
+
+$sql_rangking="SELECT no_pegawai,nama,nilai,toko,jabatan,bagian FROM rangking ORDER BY nilai DESC limit ".$jum_terbaik." ";
+$hasil_rangking=mysqli_query($db_link,$sql_rangking);
+
+
+        echo '<table class="table table-bordered table-hover text-center panel panel-primary" >
+                    
+                <thead class="panel-heading">
+                <tr>
+                    <th class="text-center" rowspan="2" style="vertical-align: middle;">RANGKING</th>
+                    <th class="text-center" rowspan="2" style="vertical-align: middle;">NO PEGAWAI</th>
+                    <th class="text-center" rowspan="2" style="vertical-align: middle;">NAMA PEGAWAI</th>
+                    <th class="text-center" rowspan="2" style="vertical-align: middle;">PAMELLA</th>
+                    <th class="text-center" rowspan="2" style="vertical-align: middle;">NILAI</th>
+                    <th class="text-center" rowspan="2" style="vertical-align: middle;">BAGIAN</th>
+                    <th class="text-center" rowspan="2" style="vertical-align: middle;">JABATAN</th>
+                </tr>
+        </thead>
+        <tbody> ';
+        $s=1;
+
+        while ($data_rangking=mysqli_fetch_assoc($hasil_rangking)) {
+            echo "<tr>";
+            echo "  
+                <td>".$s."</td>
+                 <td>{$data_rangking['no_pegawai']}</td>
+                <td>{$data_rangking['nama']}</td>
+                <td>{$data_rangking['toko']}</td>
+                <td>".$data_rangking['nilai']."</td>
+                <td>{$data_rangking['bagian']}</td>
+                <td>{$data_rangking['jabatan']}</td>";
+            echo "</tr>";
+        $s++;
+        }
+?>
